12240a8463dc050f7b2b770b334f129cb080cc56
caritogap <caritogap@gmail.com>
2026-04-24 16:08:19 -0400

    back to only vectors

--- definir_caso_base.cpp
+++ definir_caso_base.cpp
@@ -6,22 +6,20 @@
 
 int main(){
     std::ofstream timefile("strassenvsconvencionaltimes.txt");
-    
-    for (int N=4; N<512; N*=2)
+
+    for (int N=4; N<1024; N*=2)
     {
         timefile<<"N= "<<N<<std::endl;
         std::vector<float> A(N*N),B(N*N),Cstrassen(N*N),Cnormal(N*N);
-        
         for(int i=0; i<N;i++){
             for(int j=0; j<N;j++){
                 A[i*N+j]=rand();
                 B[i*N+j]=rand();
             }
         }
-        MatView Av{A,0,0,N},Bv{A,0,0,N};
         auto start = std::chrono::high_resolution_clock::now();
         for(int r=0; r<100; r++){
-            Cstrassen=strassen_mult(Av,Bv,N,4);
+            Cstrassen=strassen_mult(A,B,N,4);
         }
         auto stop = std::chrono::high_resolution_clock::now();
 
@@ -29,7 +27,7 @@ int main(){
         timefile<<"Strassen time "<<duration.count()/100<<" us"<<std::endl;
         start = std::chrono::high_resolution_clock::now();
         for(int r=0; r<100; r++){
-            Cnormal=conventional_mult(Av,Bv,N);
+            Cnormal=conventional_mult(A,B,N);
         }
         stop = std::chrono::high_resolution_clock::now();
         duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
--- strassen_secuencial.cpp
+++ strassen_secuencial.cpp
@@ -1,35 +1,13 @@
 #include <iostream>
 #include <vector>
-
-struct MatView {
-    const std::vector<float>& data;
-    int row0, col0;
-    int stride;
-
-    float operator()(int i, int j) const {
-        return data[(row0 + i) * stride + (col0 + j)];
-    }
-};
-
-struct MatRef {
-    std::vector<float>& data;
-    int row0, col0;
-    int stride;
-
-    float& operator()(int i, int j) {
-        return data[(row0 + i) * stride + (col0 + j)];
-    }
-};
-
-
-std::vector<float> conventional_mult(MatView A, MatView B, int N) {
+std::vector<float> conventional_mult(std::vector<float>& A, std::vector<float>& B, int N) {
 
     std::vector<float> C(N*N, 0.0f);
     for(int i=0; i<N; i++){ 
         for(int j=0; j<N; j++){
             float sum=0.0f;
             for(int k=0; k<N; k++){
-                sum+=(A(i,k)*B(k,j));
+                sum+=(A[i*N+k]*B[k*N+j]);
             }
             C[i*N+j]=sum;
         }
@@ -38,37 +16,36 @@ std::vector<float> conventional_mult(MatView A, MatView B, int N) {
     return C;
 }
 
-std::vector<float> strassen_mult(MatView A, MatView B, int N, int n0=4) {
+std::vector<float> strassen_mult(std::vector<float>& A, std::vector<float>& B, int N, int n0=4) {
     if(N<=n0){
         return conventional_mult(A,B,N);
     }
 
-    int h = N / 2;
-
-    MatView A11{A.data, A.row0,     A.col0,     A.stride};
-    MatView A12{A.data, A.row0,     A.col0 + h, A.stride};
-    MatView A21{A.data, A.row0 + h, A.col0,     A.stride};
-    MatView A22{A.data, A.row0 + h, A.col0 + h, A.stride};
-
-    MatView B11{B.data, B.row0,     B.col0,     B.stride};
-    MatView B12{B.data, B.row0,     B.col0 + h, B.stride};
-    MatView B21{B.data, B.row0 + h, B.col0,     B.stride};
-    MatView B22{B.data, B.row0 + h, B.col0 + h, B.stride};
+    std::vector<float> A11((N*N)/4), 
+    A12((N*N)/4),
+    A21((N*N)/4),
+    A22((N*N)/4);
 
 
-    // for(int i=0; i<N/2; i++){
-    //     for(int j=0; j<N/2; j++){
-    //         A11[i*(N/2)+j]=A[i*N+j];
-    //         A12[i*(N/2)+j]=A[i*N+(N/2)+j];
-    //         A21[i*(N/2)+j]=A[((N/2)+i)*N+j];
-    //         A22[i*(N/2)+j]=A[((N/2)+i)*N+(N/2)+j];
-    //         B11[i*(N/2)+j]=B[i*N+j];
-    //         B12[i*(N/2)+j]=B[i*N+(N/2)+j];
-    //         B21[i*(N/2)+j]=B[((N/2)+i)*N+j];
-    //         B22[i*(N/2)+j]=B[((N/2)+i)*N+(N/2)+j];
+    std::vector<float> B11((N*N)/4),
+    B12((N*N)/4),
+    B21((N*N)/4),
+    B22((N*N)/4);
+
+
+    for(int i=0; i<N/2; i++){
+        for(int j=0; j<N/2; j++){
+            A11[i*(N/2)+j]=A[i*N+j];
+            A12[i*(N/2)+j]=A[i*N+(N/2)+j];
+            A21[i*(N/2)+j]=A[((N/2)+i)*N+j];
+            A22[i*(N/2)+j]=A[((N/2)+i)*N+(N/2)+j];
+            B11[i*(N/2)+j]=B[i*N+j];
+            B12[i*(N/2)+j]=B[i*N+(N/2)+j];
+            B21[i*(N/2)+j]=B[((N/2)+i)*N+j];
+            B22[i*(N/2)+j]=B[((N/2)+i)*N+(N/2)+j];
             
-    //     }
-    // }
+        }
+    }
 
    std::vector<float> M11((N*N)/4);
    std::vector<float> M12((N*N)/4);
@@ -89,72 +66,56 @@ std::vector<float> strassen_mult(MatView A, MatView B, int N, int n0=4) {
 
    for(int i=0; i<N/2; i++){
         for(int j=0; j<N/2; j++){
-            M11[i*(N/2)+j]=A11(i,j)+A22(i,j);
-            M12[i*(N/2)+j]=B11(i,j)+B22(i,j);
-            M21[i*(N/2)+j]=A21(i,j)+A22(i,j);
-            M32[i*(N/2)+j]=B12(i,j)-B22(i,j);
-            M42[i*(N/2)+j]=B21(i,j)-B11(i,j);
-            M51[i*(N/2)+j]=A11(i,j)+A12(i,j);
-            M61[i*(N/2)+j]=A21(i,j)-A11(i,j);
-            M62[i*(N/2)+j]=B11(i,j)+B12(i,j);
-            M71[i*(N/2)+j]=A12(i,j)-A22(i,j);
-            M72[i*(N/2)+j]=B21(i,j)+B22(i,j);
+            M11[i*(N/2)+j]=A11[i*(N/2)+j]+A22[i*(N/2)+j];
+            M12[i*(N/2)+j]=B11[i*(N/2)+j]+B22[i*(N/2)+j];
+            M21[i*(N/2)+j]=A21[i*(N/2)+j]+A22[i*(N/2)+j];
+            M32[i*(N/2)+j]=B12[i*(N/2)+j]-B22[i*(N/2)+j];
+            M42[i*(N/2)+j]=B21[i*(N/2)+j]-B11[i*(N/2)+j];
+            M51[i*(N/2)+j]=A11[i*(N/2)+j]+A12[i*(N/2)+j];
+            M61[i*(N/2)+j]=A21[i*(N/2)+j]-A11[i*(N/2)+j];
+            M62[i*(N/2)+j]=B11[i*(N/2)+j]+B12[i*(N/2)+j];
+            M71[i*(N/2)+j]=A12[i*(N/2)+j]-A22[i*(N/2)+j];
+            M72[i*(N/2)+j]=B21[i*(N/2)+j]+B22[i*(N/2)+j];
             
         }
     }
-    
-    MatView M11v{M11, 0, 0, h};
-    MatView M12v{M12, 0, 0, h};
-    MatView M21v{M21, 0, 0, h};
-    MatView M32v{M32, 0, 0, h};
-    MatView M42v{M42, 0, 0, h};
-    MatView M51v{M51, 0, 0, h};
-    MatView M61v{M61, 0, 0, h};
-    MatView M62v{M62, 0, 0, h};
-    MatView M71v{M71, 0, 0, h};
-    MatView M72v{M72, 0, 0, h};
-
 
     std::vector<float> M1((N*N)/4),
     M2((N*N)/4),M3((N*N)/4),
     M4((N*N)/4),M5((N*N)/4),
     M6((N*N)/4),M7((N*N)/4);
 
-    M1=strassen_mult(M11v,M12v,N/2);
-    M2=strassen_mult(M21v,B11,N/2);
-    M3=strassen_mult(A11,M32v,N/2);
-    M4=strassen_mult(A22,M42v,N/2);
-    M5=strassen_mult(M51v,B22,N/2);
-    M6=strassen_mult(M61v,M62v,N/2);
-    M7=strassen_mult(M71v,M72v,N/2);
+    M1=strassen_mult(M11,M12,N/2);
+    M2=strassen_mult(M21,B11,N/2);
+    M3=strassen_mult(A11,M32,N/2);
+    M4=strassen_mult(A22,M42,N/2);
+    M5=strassen_mult(M51,B22,N/2);
+    M6=strassen_mult(M61,M62,N/2);
+    M7=strassen_mult(M71,M72,N/2);
 
-    std::vector<float> C(N*N);
-
-
-    MatRef C11{C, 0, 0, N};
-    MatRef C12{C, 0, h, N};
-    MatRef C21{C, h, 0, N};
-    MatRef C22{C, h, h, N};
+    std::vector<float> C11((N*N)/4),
+    C12((N*N)/4),C21((N*N)/4),
+    C22((N*N)/4);
 
     for(int i=0; i<N/2; i++){
         for(int j=0; j<N/2; j++){
-            C11(i,j)=M1[i*(N/2)+j]+M4[i*(N/2)+j]-M5[i*(N/2)+j]+M7[i*(N/2)+j];
-            C12(i,j)=M3[i*(N/2)+j]+M5[i*(N/2)+j];
-            C21(i,j)=M2[i*(N/2)+j]+M4[i*(N/2)+j];
-            C22(i,j)=M1[i*(N/2)+j]-M2[i*(N/2)+j]+M3[i*(N/2)+j]+M6[i*(N/2)+j];           
+            C11[i*(N/2)+j]=M1[i*(N/2)+j]+M4[i*(N/2)+j]-M5[i*(N/2)+j]+M7[i*(N/2)+j];
+            C12[i*(N/2)+j]=M3[i*(N/2)+j]+M5[i*(N/2)+j];
+            C21[i*(N/2)+j]=M2[i*(N/2)+j]+M4[i*(N/2)+j];
+            C22[i*(N/2)+j]=M1[i*(N/2)+j]-M2[i*(N/2)+j]+M3[i*(N/2)+j]+M6[i*(N/2)+j];           
         }
     }
 
-   
+    std::vector<float> C(N*N);
 
-    // for(int i=0; i<N/2; i++){
-    //     for(int j=0; j<N/2; j++){
-    //         C[i*N+j]=C11[i*(N/2)+j];
-    //         C[((N/2)+i)*N+j]=C21[i*(N/2)+j];
-    //         C[i+(N/2)+j]=C12[i*(N/2)+j];
-    //         C[((N/2)+i)*N+(N/2)+j]=C22[i*(N/2)+j];       
-    //     }
-    // }
+    for(int i=0; i<N/2; i++){
+        for(int j=0; j<N/2; j++){
+            C[i*N+j]=C11[i*(N/2)+j];
+            C[((N/2)+i)*N+j]=C21[i*(N/2)+j];
+            C[i+(N/2)+j]=C12[i*(N/2)+j];
+            C[((N/2)+i)*N+(N/2)+j]=C22[i*(N/2)+j];       
+        }
+    }
 
     return C;
 }
