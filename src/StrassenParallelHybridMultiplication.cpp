ae1d3112b5ad0008961d32d2a66272dd067a4a72
Yian-n <gbarbagelatah@gmail.com>
2026-04-26 22:03:48 -0400

    Hybrido y test suite

---
+++ src/StrassenParallelHybridMultiplication.cpp
@@ -0,0 +1,132 @@
+#include <vector>
+#include <omp.h>
+#include <vector>
+
+
+template <class T>
+void tile_mult_parallel(const std::vector<T>& A,
+                        const std::vector<T>& B,
+                        std::vector<T>& C,
+                        size_t n,
+                        size_t block_size=16) {
+    for (size_t i=0; i<n*n; i++)
+        C[i] = 0;
+
+    #pragma omp parallel for collapse(2) schedule(static)
+    for (size_t tile_i=0; tile_i < n; tile_i+=block_size)
+        for (size_t tile_k=0; tile_k < n; tile_k+=block_size)
+            for (size_t tile_j=0; tile_j < n; tile_j+=block_size)
+                for (size_t i=tile_i; i<tile_i+block_size; ++i)
+                    for (size_t k=tile_k; k<tile_k+block_size; ++k)
+                        for (size_t j=tile_j; j<tile_j+block_size; ++j)
+                            C[i*n+j] += A[i*n+k] * B[k*n+j];
+}
+
+
+template <class T>
+void strassen_mult_parallel(const std::vector<T>& A,
+                            const std::vector<T>& B,
+                            std::vector<T>& C,
+                            size_t N,
+                            int n0=64) {
+    if(N<=n0){
+        return tile_mult_parallel(A,B,C,N);
+    }
+
+    std::vector<T> A11((N*N)/4), 
+                   A12((N*N)/4),
+                   A21((N*N)/4),
+                   A22((N*N)/4);
+
+    std::vector<T> B11((N*N)/4),
+                   B12((N*N)/4),
+                   B21((N*N)/4),
+                   B22((N*N)/4);
+
+    for(int i=0; i<N/2; i++){
+        for(int j=0; j<N/2; j++){
+            A11[i*(N/2)+j]=A[i*N+j];
+            A12[i*(N/2)+j]=A[i*N+(N/2)+j];
+            A21[i*(N/2)+j]=A[((N/2)+i)*N+j];
+            A22[i*(N/2)+j]=A[((N/2)+i)*N+(N/2)+j];
+            B11[i*(N/2)+j]=B[i*N+j];
+            B12[i*(N/2)+j]=B[i*N+(N/2)+j];
+            B21[i*(N/2)+j]=B[((N/2)+i)*N+j];
+            B22[i*(N/2)+j]=B[((N/2)+i)*N+(N/2)+j];
+        }
+    }
+
+    std::vector<T> M11((N*N)/4);
+    std::vector<T> M12((N*N)/4);
+
+    std::vector<T> M21((N*N)/4);
+
+    std::vector<T> M32((N*N)/4);
+
+    std::vector<T> M42((N*N)/4);
+
+    std::vector<T> M51((N*N)/4) ;
+
+    std::vector<T> M61((N*N)/4);
+    std::vector<T> M62((N*N)/4);
+
+    std::vector<T> M71((N*N)/4);
+    std::vector<T> M72((N*N)/4);
+
+    for(int i=0; i<N/2; i++){
+        for(int j=0; j<N/2; j++){
+            M11[i*(N/2)+j]=A11[i*(N/2)+j]+A22[i*(N/2)+j];
+            M12[i*(N/2)+j]=B11[i*(N/2)+j]+B22[i*(N/2)+j];
+            M21[i*(N/2)+j]=A21[i*(N/2)+j]+A22[i*(N/2)+j];
+            M32[i*(N/2)+j]=B12[i*(N/2)+j]-B22[i*(N/2)+j];
+            M42[i*(N/2)+j]=B21[i*(N/2)+j]-B11[i*(N/2)+j];
+            M51[i*(N/2)+j]=A11[i*(N/2)+j]+A12[i*(N/2)+j];
+            M61[i*(N/2)+j]=A21[i*(N/2)+j]-A11[i*(N/2)+j];
+            M62[i*(N/2)+j]=B11[i*(N/2)+j]+B12[i*(N/2)+j];
+            M71[i*(N/2)+j]=A12[i*(N/2)+j]-A22[i*(N/2)+j];
+            M72[i*(N/2)+j]=B21[i*(N/2)+j]+B22[i*(N/2)+j];
+        }
+    }
+
+    std::vector<T> M1((N*N)/4),
+                   M2((N*N)/4),
+                   M3((N*N)/4),
+                   M4((N*N)/4),
+                   M5((N*N)/4),
+                   M6((N*N)/4),
+                   M7((N*N)/4);
+
+    #pragma omp parallel
+    #pragma omp single
+    #pragma omp taskgroup
+    {
+        #pragma omp task shared(M1)
+        strassen_mult_parallel(M11, M12, M1, N/2);
+        #pragma omp task shared(M2)
+        strassen_mult_parallel(M21, B11, M2, N/2);
+        #pragma omp task shared(M3)
+        strassen_mult_parallel(A11, M32, M3, N/2);
+        #pragma omp task shared(M4)
+        strassen_mult_parallel(A22, M42, M4, N/2);
+        #pragma omp task shared(M5)
+        strassen_mult_parallel(M51, B22, M5, N/2);
+        #pragma omp task shared(M6)
+        strassen_mult_parallel(M61, M62, M6, N/2);
+        #pragma omp task shared(M7)
+        strassen_mult_parallel(M71, M72, M7, N/2);
+    }
+
+    std::vector<T> C11((N*N)/4),
+                   C12((N*N)/4),
+                   C21((N*N)/4),
+                   C22((N*N)/4);
+
+    for(int i=0; i<N/2; i++){
+        for(int j=0; j<N/2; j++){
+            C[i*N+j]          = M1[i*(N/2)+j]+M4[i*(N/2)+j]-M5[i*(N/2)+j]+M7[i*(N/2)+j];
+            C[i*N+j+N/2]      = M3[i*(N/2)+j]+M5[i*(N/2)+j];
+            C[(i+N/2)*N+j]    = M2[i*(N/2)+j]+M4[i*(N/2)+j];
+            C[(i+N/2)*N+j+N/2]= M1[i*(N/2)+j]-M2[i*(N/2)+j]+M3[i*(N/2)+j]+M6[i*(N/2)+j];           
+        }
+    }
+}
---
+++ test_suite/parallel/strassen_hybrid_test.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+#include "../../src/StrassenParallelHybridMultiplication.cpp"
+#include "../../src/matrix.cpp"
+
+int main(int argn, char** argv) {
+    int N = atoi(argv[1]);
+    int n0 = atoi(argv[2]);
+
+    // Tecnicamente, un bloque de memoria sin inicializar es random :p
+    std::vector<float> A(N*N);
+    std::vector<float> B(N*N);
+    std::vector<float> C(N*N);
+    
+    double time = medir([&]() { strassen_mult_parallel(A, B, C, N, n0); });
+    std::cout << "Measure time: " << time << std::endl;
+
+    return 0;
+}
